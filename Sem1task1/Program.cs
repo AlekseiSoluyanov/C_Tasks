426db25ea143e71d635d1f312ba3744586915e2f
Aleksey <alekc.col@mail.ru>
2022-11-30 17:32:29 +0300

    Sem1Task1 added

---
+++ Sem1task1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Считываем данные с консоил
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+//Проверяем чтобы данные были не пустыми
+if(num1Line!=null && num2Line!=null)
+{
+    // Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    //Находим является ли первое число квадрат второго
+    //Выводим данные в консоль
+
+    if (num2*num2 == num1)
+    {
+        Console.WriteLine("Первое число квадрат второго!");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не квадрат второго!");
+    }
+}
