81ac7d72a205cefb508cdbf5e89d3a69240c1b8c
Aleksey <alekc.col@mail.ru>
2022-12-01 14:44:10 +0300

    Add solution fot task 0, 1, 3, 5, 7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе
+// показывает последнюю цифру этого числа.
+
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+// Проверям, чтобы данные были не пустыми
+if (inputLine != null)
+{   
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    int lastDigit = inputNumber % 10;
+    
+    Console.WriteLine(lastDigit);
+}
---
+++ Sem1Tast5/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+// Проверям, чтобы данные были не пустыми
+if (inputLine != null)
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    int startNumber = inputNumber*(-1);
+
+    //Выходное значение (где будем накапливать результат)
+    string outLine = string.Empty;
+
+    //создаем цикл для набора значний
+    while(startNumber<inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        startNumber++; //startNumber=startNumber+1;   
+    }
+  outLine = outLine + inputNumber;
+
+  //Выводим данные в консоль
+  Console.WriteLine(outLine);
+}
