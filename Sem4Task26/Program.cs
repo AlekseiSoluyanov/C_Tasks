9536f3a7330bd294c898eff1141c90f845f4136f
Aleksey <alekc.col@mail.ru>
2022-12-13 11:42:40 +0300

    Add solution for task 24, 26, 28, 30.

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая принимает на вход
+// число и выдаёт количество цифр в числе.
+
+//Запрос данных у пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод вывода рузльтата
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int VariantLog(int num)
+{
+    int count = (int)Math.Log10(num)+1;
+    return count;
+}
+
+int VariantString(int num)
+{
+    int count = num.ToString().Length;
+    return count;
+}
+
+int VariantSimple(int num)
+{
+    int count = 0;
+    while(num>0)
+    {
+        count++;
+        num=num/10;
+    }
+    return count;
+}
+
+int number = ReadData("Введите число А:");
+
+DateTime d1 = DateTime.Now;
+int lenght1 = VariantLog(number);
+Console.WriteLine(DateTime.Now - d1);
+
+DateTime d2 = DateTime.Now;
+int lenght2 = VariantString(number);
+Console.WriteLine(DateTime.Now - d2);
+
+DateTime d3 = DateTime.Now;
+int lenght3 = VariantSimple(number);
+Console.WriteLine(DateTime.Now - d3);
+
+PrintData("Колличество цифр в числе равно: " + lenght1);
+PrintData("Колличество цифр в числе равно: " + lenght2);
+PrintData("Колличество цифр в числе равно: " + lenght3);
---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N. (Факториал)
+
+//Запрос данных у пользователя
+
+using System.Numerics;
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод вывода рузльтата
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+BigInteger CalcFactor(int num)
+{
+    BigInteger count = 1;
+    for(int i=1;i<=num;i++)
+    {
+        count *= i;
+    }
+    return count;
+}
+
+int number = ReadData("Введите число А:");
+
+BigInteger lenght1 = CalcFactor(number);
+
+PrintData("Факториал равно: " + lenght1);
