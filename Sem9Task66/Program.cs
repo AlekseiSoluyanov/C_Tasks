0386a4318102f08d3d6372788c5d1ebec0563b01
Aleksey <alekc.col@mail.ru>
2022-12-27 16:44:44 +0300

    Add solution for task 64, 66, 68.

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод вывода данных
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Метод находит сумму натуральных элементов в промежутке от M до N.
+int SumRecMN(int M, int N)
+{
+    if (M >= N) return N;
+    else return M + SumRecMN(M + 1, N);
+}
+
+int number1 = ReadData("Введите число M: ");
+int number2 = ReadData("Введите число N: ");
+int result = SumRecMN(number1, number2);
+PrintData("Сумма элменетов равна: " + result);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+int numM = ReadData("Введите число M: ");
+int numN = ReadData("Введите число N: ");
+int result = AkkerFunk(numM, numN);
+PrintData("Функция Аккермана равна: "+result);
+
+// Функция Аккермана
+int AkkerFunk(int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return AkkerFunk(m - 1, 1);
+    else
+      return AkkerFunk(m - 1, AkkerFunk(m, n - 1));
+}
+
+// Метод вывода данных.
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Чтение данных из консоли.
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
